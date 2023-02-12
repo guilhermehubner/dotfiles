0288142710180e69057e14b6c1eb7123102a0cf4
Guilherme Hübner <guilhermehubner@gmail.com>
2023-02-12 16:46:02 +0100

    tree-sitter hcl support

--- nvim/plugin/nvim-treesitter.lua
+++ nvim/plugin/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = {
-        'help', 'go', 'lua', 'javascript', 'tsx', 'json', 'yaml', 'html', 'css', 'typescript', 'python'
+        'help', 'go', 'lua', 'javascript', 'tsx', 'json', 'yaml', 'html', 'css', 'typescript',
+        'python', 'hcl'
     },
 
     highlight = {enable = true},
