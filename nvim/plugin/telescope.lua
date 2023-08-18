be39416d4633f2d56fc30cbcb83ba2df8d5fd2fa
Guilherme Hübner <guilhermehubner@gmail.com>
2023-08-18 16:55:14 +0200

    telescope ignore vendor

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -6,3 +6,9 @@ vim.api.nvim_set_keymap('n', '<leader>F', ':Telescope current_buffer_fuzzy_find<
 vim.api.nvim_set_keymap('n', '<leader>ts', ':Telescope treesitter<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>tp', ':Telescope resume<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>te', ':Telescope diagnostics<CR>', {})
+
+require('telescope').setup{
+	defaults = {
+		file_ignore_patterns = { "^vendor/" }
+	}
+}
