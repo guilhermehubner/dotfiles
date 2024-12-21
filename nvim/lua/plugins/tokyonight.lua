e42bbacdb92846aea95d910248a5aefc2f413b6d
Guilherme Hübner <guilhermehubner@gmail.com>
2024-12-21 15:44:58 +0100

    [nvim] transparent bg

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -5,5 +5,10 @@ return {
     config = function()
         vim.o.termguicolors = true
         vim.cmd([[colorscheme tokyonight-storm]])
+
+        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
     end,
 }
